d76b4ed451a5cd8d3d6e3fafaa2d5d8b4a91af48
Mythic <mythicmaniac@gmail.com>
2023-07-18 04:02:31 +0300

    Generate test data

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+from base64 import b64encode
+from csv import DictWriter
+
 from b64_regex.generate_test import generate_flags, wrap_with_random_text
 
 
-def main():
+def dump_test_flags_to_file(destination: str):
+    with open(destination, "w", newline="") as f:
+        writer = DictWriter(f, fieldnames=["flag", "wrapped", "encoded"])
+        flags = generate_flags(1000)
+        writer.writeheader()
+        for flag, wrapped in zip(flags, wrap_with_random_text(flags)):
+            encoded = b64encode(wrapped.encode()).decode()
+            writer.writerow(
+                {
+                    "flag": flag,
+                    "wrapped": wrapped,
+                    "encoded": encoded,
+                }
+            )
+
+
+def print_test_flags():
     flags = generate_flags()
     for flag, wrapped in zip(flags, wrap_with_random_text(flags)):
-        print(f"{flag=} | {wrapped=}")
+        encoded = b64encode(wrapped.encode())
+        print(f"{flag=} | {wrapped=} | {encoded=}")
+
+
+def main():
+    dump_test_flags_to_file("./test.csv")
 
 
 if __name__ == "__main__":
