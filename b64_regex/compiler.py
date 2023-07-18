8709f22993b43b87e3cf6cc3f84dbbd182b2af7c
Mythic <mythicmaniac@gmail.com>
2023-07-18 05:29:37 +0300

    Improve PoC

--- b64_regex/compiler.py
+++ b64_regex/compiler.py
@@ -5,13 +5,14 @@ from bitarray import bitarray
 
 
 def make_alignment_variants(known: str) -> List[str]:
-    if len(known) < 3:
-        raise AttributeError("Search string must be at least 3 characters long")
+    if len(known) < 2:
+        raise AttributeError("Search string must be at least 2 characters long")
 
     byted = known.encode()
-    variant1 = b64encode(byted).decode().replace("=", "")
-    variant2 = b64encode(f"aa{known}".encode()).decode().replace("=", "")[3:-1]
-    variant3 = b64encode(f"a{known}".encode()).decode().replace("=", "")[2:-1]
+    safe_tokens = (len(byted) * 8 // 6) - 1
+    variant1 = b64encode(byted).decode()[:safe_tokens]
+    variant2 = b64encode(f"aa{known}".encode()).decode()[3 : 3 + safe_tokens]
+    variant3 = b64encode(f"a{known}".encode()).decode()[2 : 2 + safe_tokens]
     return [variant1, variant2, variant3]
 
 
