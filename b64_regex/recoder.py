6a0baf88c77f411fca74b898dde5dc7cc57c2ca0
Mythic <mythicmaniac@gmail.com>
2023-07-18 07:52:05 +0300

    Working PoC regex building

--- b64_regex/recoder.py
+++ b64_regex/recoder.py
@@ -1,7 +1,45 @@
 import string
+from typing import Literal, Iterable
 
 CHARSET = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/"
 CHARSET_REVERSE = {v: k for k, v in enumerate(CHARSET)}
+
+
+Alignment = Literal[0, 2, 4]
+
+
+def stupid_pow(a: int, b: int) -> int:
+    if b == 0:
+        return 0
+    return pow(a, b)
+
+
+class TokenSequence:
+    bits: str
+
+    def __init__(self, bits: str):
+        self.bits = bits
+
+    def with_alignment(self, alignment: Alignment) -> Iterable[str]:
+        prefix_bits = alignment
+        suffix_bits = 6 - ((len(self.bits) + alignment) % 6 or 6)
+        variant_bits = prefix_bits + suffix_bits
+        if variant_bits not in (0, 2, 4, 6):
+            raise RuntimeError(f"Math doesn't check out: {variant_bits}")
+
+        if variant_bits == 0:
+            yield self.bits
+
+        for i in range(stupid_pow(2, variant_bits)):
+            padding_bits = f"{i:b}".zfill(variant_bits)
+            prefix = padding_bits[:prefix_bits]
+            suffix = padding_bits[prefix_bits:]
+            yield f"{prefix}{self.bits}{suffix}"
+
+    def with_all_alignments(self) -> Iterable[str]:
+        for alignment in (0, 2, 4):
+            for entry in self.with_alignment(alignment):
+                yield entry
 
 
 def iter_windows(window_size: int, arr):
@@ -9,20 +47,30 @@ def iter_windows(window_size: int, arr):
         yield arr[index : index + window_size]
 
 
-def base64_encode(content: bytes) -> str:
-    bits = "".join([f"{x:08b}" for x in content])
+def bytes_to_bits(content: bytes) -> str:
+    return "".join([f"{x:08b}" for x in content])
 
-    if padding_bits := 6 - (len(bits) % 6):
+
+def base64_encode(content: bytes) -> str:
+    encoded = b64_encode_bits_without_padding(bytes_to_bits(content))
+    return add_b64_padding(encoded)
+
+
+def b64_encode_bits_without_padding(bits: str) -> str:
+    if padding_bits := 6 - ((len(bits) % 6) or 6):
         bits += padding_bits * "0"
 
     result = ""
     for window in iter_windows(6, bits):
         result += CHARSET[int(window, 2)]
 
-    if padding_chars := 4 - (len(result) % 4):
-        result += "=" * padding_chars
-
     return result
+
+
+def add_b64_padding(encoded: str) -> str:
+    if padding_chars := 4 - ((len(encoded) % 4) or 4):
+        encoded += "=" * padding_chars
+    return encoded
 
 
 def base64_decode(content: str) -> bytes:
@@ -30,8 +78,3 @@ def base64_decode(content: str) -> bytes:
     token_bytes = [CHARSET_REVERSE[x] for x in stripped]
     bits = "".join(f"{x:06b}" for x in token_bytes)
     return bytes(int(x, 2) for x in iter_windows(8, bits) if len(x) == 8)
-
-
-class Recoder:
-    def __init__(self):
-        pass
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from base64 import b64encode
 from csv import DictWriter
+from typing import List
 
 from b64_regex.compiler import make_alignment_variants
 from b64_regex.generate_test import generate_flags, wrap_with_random_text
-from b64_regex.recoder import base64_encode, base64_decode
+from b64_regex.recoder import (
+    base64_encode,
+    base64_decode,
+    TokenSequence,
+    bytes_to_bits,
+    b64_encode_bits_without_padding,
+)
 
 
-def dump_test_flags_to_file(destination: str):
+def dump_test_flags_to_csv(destination: str):
     with open(destination, "w", newline="") as f:
         writer = DictWriter(f, fieldnames=["flag", "wrapped", "encoded"])
         flags = generate_flags(1000)
@@ -22,6 +29,15 @@ def dump_test_flags_to_file(destination: str):
             )
 
 
+def dump_flags_to_txt(destination: str):
+    with open(destination, "w") as f:
+        flags = generate_flags(1000)
+        lines = [
+            b64encode(x.encode()).decode() + "\n" for x in wrap_with_random_text(flags)
+        ]
+        f.writelines(lines)
+
+
 def print_test_flags():
     flags = generate_flags()
     for flag, wrapped in zip(flags, wrap_with_random_text(flags)):
@@ -33,15 +49,41 @@ def build_regex():
     print(make_alignment_variants("HTB{"))
 
 
+def variants_to_regex(variants: List[str]) -> str:
+    return "|".join(variants).replace("/", "\\/").replace("+", "\\+")
+
+
 def main():
+    # dump_flags_to_txt("./test.txt")
     # dump_test_flags_to_file("./test.csv")
     # build_regex()
-    from base64 import b64encode
+    # from base64 import b64encode
+    #
+    # print(b64encode(b"Hello"))
+    # encoded = base64_encode(b"Hello")
+    # print(encoded)
+    # print(base64_decode(encoded))
 
-    print(b64encode(b"Hello"))
-    encoded = base64_encode(b"Hello")
-    print(encoded)
-    print(base64_decode(encoded))
+    # seq = TokenSequence(bytes_to_bits(b"Three days grade school,"))
+    # vars = [b64_encode_bits_without_padding(x) for x in seq.with_all_alignments()]
+    # regx = variants_to_regex(vars)
+    # print(regx)
+
+    start_seq = TokenSequence(bytes_to_bits(b"HTB{"))
+    start_variants = [
+        b64_encode_bits_without_padding(x) for x in start_seq.with_all_alignments()
+    ]
+    start_regex = variants_to_regex(start_variants)
+
+    end_seq = TokenSequence(bytes_to_bits(b"}"))
+    end_variants = [
+        b64_encode_bits_without_padding(x) for x in end_seq.with_all_alignments()
+    ]
+    end_regex = variants_to_regex(end_variants)
+
+    full_regex = f"({start_regex})" "[a-zA-Z0-9\\/\\+]+" f"({end_regex})"
+
+    print(full_regex)
 
 
 if __name__ == "__main__":
